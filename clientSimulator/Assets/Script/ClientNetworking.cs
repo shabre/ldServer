fc979aac1789e99d47e162de33f4c062aa5dcbc7
shabre <shabreking92@gmail.com>
2017-08-02 16:19:06 +0900

    8_2 update

--- clientSimulator/Assets/Script/ClientNetworking.cs
+++ clientSimulator/Assets/Script/ClientNetworking.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using PacketProtocols;
+using System.Threading;
 
 //서버와의 네트워킹을 담당하는 클래스
 public class ClientNetworking : MonoBehaviour {
@@ -16,12 +17,14 @@ public class ClientNetworking : MonoBehaviour {
 	void Start () {
                 client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 client.Connect(ipep);
+                ThreadStart ts=new ThreadStart(receivePacket);
+                Thread tid=new Thread(ts);
+                tid.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
                 short len=0;
-                int nbyte;
                 tr=this.gameObject.GetComponent<Transform>();
                 
                 short request =1;
@@ -37,9 +40,8 @@ public class ClientNetworking : MonoBehaviour {
 
                 
                 byte[] sbuf=pPacket.packetsToByte();
-                byte[] rbuf=new byte[512];
                 client.Send(sbuf);
-     
+                /* 
                 nbyte=client.Receive(rbuf);
                 Pos_Packet rPacket=new Pos_Packet(rbuf);
                 Debug.Log(nbyte+" byte received");
@@ -47,10 +49,23 @@ public class ClientNetworking : MonoBehaviour {
                         +" y: "+rPacket.getY() +" z: "+rPacket.getZ());
                 
                 //client.Close();
+                */
 	}
-
-        
         void onDestroy(){
                 client.Close();
+        }
+
+        void receivePacket(){
+                int nbyte;
+                byte[] rbuf=new byte[512];
+                while(true){
+                        nbyte=client.Receive(rbuf);
+                        Pos_Packet rPacket=new Pos_Packet(rbuf);
+                        Debug.Log(nbyte+" byte received");
+                        Debug.Log(rPacket.getID()+" x: "+rPacket.getX()
+                                +" y: "+rPacket.getY() +" z: "+rPacket.getZ());
+                }
+                
+                
         }
 }
